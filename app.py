24176a1f9b2a4fb40a6d4513e4f51c371096648a
Rparizi <parizirb@gmail.com>
2024-03-19 13:50:38 -0300

    updating app.py

--- app.py
+++ app.py
@@ -217,7 +217,8 @@ def about():
     dados_acesso = extrair_dados(access_token)
     return render_template("/pages/about.html", dados_usuario=dados_acesso)
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
